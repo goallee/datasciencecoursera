40476a2b6a60ba6528ca9e2da4502100105b93eb
goallee@gmail.com <goallee@gmail.com>
2014-03-29 15:26:47 -0700

    initial commit

---
+++ PasadenaIO/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PasadenaIO::Application.config.secret_key_base = 'b77f76060d2ce299c0bc39fce53e2d52c1b7a0041ff206d39e80ea283c565cc45e255b7214d2b983a9ed93d48c23941765490511e6c4e6a1240bb62479ea26f5'
